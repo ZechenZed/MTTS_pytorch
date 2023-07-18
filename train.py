a0e3b2922a13c4ab0a9f00f4f16d29a9e4b7ac0b
ZechenZed <fluffyzed2@gmail.com>
2023-07-18 15:21:17 -0400

    Add plot_prediction.py and successfully trained the model

--- train.py
+++ train.py
@@ -9,99 +9,138 @@ from neural_methods.model.TS_CAN import TSCAN
 from data_loader import V4V_Dataset
 from tqdm import tqdm
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-exp', '--exp_type', type=str, default='video',
-                    help='experiment type: model or video')
-parser.add_argument('-data', '--data_type', type=str, default='train',
-                    help='data type')
-parser.add_argument('-BP', '--BP_type', type=str, default='systolic',
-                    help='Choose type of BP from mean, systolic and diastolic')
-parser.add_argument('-image', '--image_type', type=str, default='face_large',
-                    help='choose from 1) ratio, 2) face_large, 3) face')
-parser.add_argument('-device', '--device_type', type=str, default='remote',
-                    help='Local / Remote device')
-parser.add_argument('-g', '--nb_epoch', type=int, default=30,
-                    help='nb_epoch')
-parser.add_argument('--nb_batch', type=int, default=6,
-                    help='nb_batch')
-parser.add_argument('--gpu', type=str, default='0',
-                    help='List of GPUs used')
-parser.add_argument('--nb_device', type=int, default=1,
-                    help='Total number of device')
-parser.add_argument('-lr', '--lr', type=float, default=9e-3,
-                    help='learning rate')
-parser.add_argument('-fd', '--frame_depth', type=int, default=10,
-                    help='frame depth')
-args = parser.parse_args()
-print('input args:\n', json.dumps(vars(args), indent=4, separators=(',', ':')))  # pretty print args
 
-os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+class TSCAN_trainer:
+    def __init__(self, setup):
+        ################### Env setup ###################
+        os.environ['CUDA_VISIBLE_DEVICES'] = setup.gpu
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.frame_depth = setup.frame_depth
+        self.nb_epoch = setup.nb_epoch
+        self.lr = setup.lr
+        self.criterion = torch.nn.MSELoss()
+        self.min_valid_loss = None
+        self.best_epoch = 0
+        self.model_dir = 'C:/Users/Zed/Desktop/MTTS_pytorch/model_ckpts/'
+        self.model_file_name = f'TSCAN_{setup.image_type}'
+        self.base_len = setup.nb_device * self.frame_depth
+        self.batch_size = setup.nb_batch
+        ################### Load data ###################
+        if setup.device_type == 'local':
+            data_folder_path = 'C:/Users/Zed/Desktop/V4V/preprocessed_v4v/'
+        else:
+            data_folder_path = '/edrive2/zechenzh/preprocessed_v4v/'
+        v4v_data_train = V4V_Dataset(data_folder_path, setup.data_type, setup.image_type, setup.BP_type)
+        v4v_data_valid = V4V_Dataset(data_folder_path, 'valid', setup.image_type, setup.BP_type)
+        self.train_loader = DataLoader(dataset=v4v_data_train, batch_size=self.batch_size, shuffle=True, num_workers=0)
+        self.valid_loader = DataLoader(dataset=v4v_data_valid, batch_size=self.batch_size, shuffle=True, num_workers=0)
 
-if args.device_type == 'local':
-    data_folder_path = 'C:/Users/Zed/Desktop/V4V/preprocessed_v4v/'
-else:
-    data_folder_path = '/edrive2/zechenzh/preprocessed_v4v/'
+        ################### Model Init ###################
+        model = TSCAN(frame_depth=self.frame_depth, img_size=72).to(self.device)
+        self.model = torch.nn.DataParallel(model, device_ids=list(range(setup.nb_device)))
+        self.optimizer = optim.AdamW(model.parameters(), lr=self.lr, weight_decay=0)
+        self.scheduler = torch.optim.lr_scheduler.OneCycleLR(self.optimizer, max_lr=self.lr, epochs=self.nb_epoch,
+                                                             steps_per_epoch=len(self.train_loader))
 
-################### Load data ###################
-v4v_data = V4V_Dataset(data_folder_path, args.data_type, args.image_type, args.BP_type)
-train_loader = DataLoader(dataset=v4v_data, batch_size=4, shuffle=True, num_workers=0)
-# dataiter = iter(train_loader)
-# data = next(dataiter)
-# frame, BP = data
-# print(frame, frame.shape)
-# print(BP, BP.shape)
-
-################### Model Init ###################
-model = TSCAN(frame_depth=args.frame_depth, img_size=72).to(device)
-model = torch.nn.DataParallel(model, device_ids=list(range(args.nb_device)))
-base_len = args.nb_device * args.frame_depth
-criterion = torch.nn.MSELoss()
-optimizer = optim.AdamW(model.parameters(), lr=0.01, weight_decay=0)
-scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=args.lr, epochs=args.nb_epoch,
-                                                steps_per_epoch=len(train_loader))
-for epoch in range(args.nb_epoch):
-    print('')
-    print(f"====Training Epoch: {epoch}====")
-    running_loss = 0.0
-    train_loss = []
-    model.train()
-    # Model Training
-    tbar = tqdm(train_loader, ncols=80)
-    for idx, batch in enumerate(tbar):
-        tbar.set_description("Train epoch %s" % epoch)
-        data, labels = batch[0].to(
-            device), batch[1].to(device)
-        N, D, C, H, W = data.shape
-        data = data.view(N * D, C, H, W)
-        labels = labels.view(-1, 1)
-        data = data[:(N * D) // base_len * base_len]
-        labels = labels[:(N * D) // base_len * base_len]
-        optimizer.zero_grad()
-        pred_ppg = model(data)
-        loss = criterion(pred_ppg, labels)
-        loss.backward()
-        optimizer.step()
-        scheduler.step()
-        running_loss += loss.item()
-        if idx % 100 == 99:  # print every 100 mini-batches
-            print(
-                f'[{epoch}, {idx + 1:5d}] loss: {running_loss / 100:.3f}')
+    def train(self):
+        for epoch in range(self.nb_epoch):
+            print('')
+            print(f"====Training Epoch: {epoch}====")
             running_loss = 0.0
-        train_loss.append(loss.item())
-        tbar.set_postfix(loss=loss.item())
-#     save_model(epoch)
-#     if not config.TEST.USE_LAST_EPOCH:
-#         valid_loss = valid(data_loader)
-#         print('validation loss: ', valid_loss)
-#         if min_valid_loss is None:
-#             min_valid_loss = valid_loss
-#             best_epoch = epoch
-#             print("Update best model! Best epoch: {}".format(best_epoch))
-#         elif (valid_loss < min_valid_loss):
-#             min_valid_loss = valid_loss
-#             best_epoch = epoch
-#             print("Update best model! Best epoch: {}".format(best_epoch))
-# if not config.TEST.USE_LAST_EPOCH:
-#     print("best trained epoch: {}, min_val_loss: {}".format(best_epoch, min_valid_loss))
+            train_loss = []
+            self.model.train()
+            # Model Training
+            tbar = tqdm(self.train_loader, ncols=80)
+            for idx, batch in enumerate(tbar):
+                tbar.set_description("Train epoch %s" % epoch)
+                data, labels = batch[0].to(self.device), batch[1].to(self.device)
+                N, D, C, H, W = data.shape
+                data = data.view(N * D, C, H, W)
+                labels = labels.view(-1, 1)
+                data = data[:(N * D) // self.base_len * self.base_len]
+                labels = labels[:(N * D) // self.base_len * self.base_len]
+                self.optimizer.zero_grad()
+                pred_ppg = self.model(data)
+                loss = self.criterion(pred_ppg, labels)
+                loss.backward()
+                self.optimizer.step()
+                self.scheduler.step()
+                running_loss += loss.item()
+                if idx % 100 == 99:  # print every 100 mini-batches
+                    print(
+                        f'[{epoch}, {idx + 1:5d}] loss: {running_loss / 100:.3f}')
+                    running_loss = 0.0
+                train_loss.append(loss.item())
+                tbar.set_postfix(loss=loss.item())
+            self.save_model(epoch)
+            valid_loss = self.valid(self.valid_loader)
+            print('validation loss: ', valid_loss)
+            if self.min_valid_loss is None:
+                self.min_valid_loss = valid_loss
+                self.best_epoch = 0
+                print("Update best model! Best epoch: {}".format(self.best_epoch))
+            elif valid_loss < self.min_valid_loss:
+                self.min_valid_loss = valid_loss
+                self.best_epoch = epoch
+                print("Update best model! Best epoch: {}".format(self.best_epoch))
+        print("best trained epoch: {}, min_val_loss: {}".format(self.best_epoch, self.min_valid_loss))
 
+    def valid(self, data_loader):
+        print('')
+        print("===Validating===")
+        valid_loss = []
+        self.model.eval()
+        valid_step = 0
+        with torch.no_grad():
+            vbar = tqdm(data_loader, ncols=80)
+            for valid_idx, valid_batch in enumerate(vbar):
+                vbar.set_description("Validation")
+                data_valid, labels_valid = valid_batch[0].to(
+                    self.device), valid_batch[1].to(self.device)
+                N, D, C, H, W = data_valid.shape
+                data_valid = data_valid.view(N * D, C, H, W)
+                labels_valid = labels_valid.view(-1, 1)
+                data_valid = data_valid[:(N * D) // self.base_len * self.base_len]
+                labels_valid = labels_valid[:(N * D) // self.base_len * self.base_len]
+                pred_ppg_valid = self.model(data_valid)
+                loss = self.criterion(pred_ppg_valid, labels_valid)
+                valid_loss.append(loss.item())
+                valid_step += 1
+                vbar.set_postfix(loss=loss.item())
+            valid_loss = np.asarray(valid_loss)
+        return np.mean(valid_loss)
+
+    def save_model(self, index):
+        model_path = os.path.join(self.model_dir, self.model_file_name + '_Epoch' + str(index) + '.pth')
+        torch.save(self.model.state_dict(), model_path)
+        print('Saved Model Path: ', model_path)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-exp', '--exp_type', type=str, default='video',
+                        help='experiment type: model or video')
+    parser.add_argument('-data', '--data_type', type=str, default='train',
+                        help='data type')
+    parser.add_argument('-BP', '--BP_type', type=str, default='systolic',
+                        help='Choose type of BP from mean, systolic and diastolic')
+    parser.add_argument('-image', '--image_type', type=str, default='face_large',
+                        help='choose from 1) ratio, 2) face_large, 3) face')
+    parser.add_argument('-device', '--device_type', type=str, default='local',
+                        help='Local / Remote device')
+    parser.add_argument('-g', '--nb_epoch', type=int, default=50,
+                        help='nb_epoch')
+    parser.add_argument('--nb_batch', type=int, default=4,
+                        help='nb_batch')
+    parser.add_argument('--gpu', type=str, default='0',
+                        help='List of GPUs used')
+    parser.add_argument('--nb_device', type=int, default=1,
+                        help='Total number of device')
+    parser.add_argument('-lr', '--lr', type=float, default=0.01,
+                        help='learning rate')
+    parser.add_argument('-fd', '--frame_depth', type=int, default=10,
+                        help='frame depth')
+    args = parser.parse_args()
+    print('input args:\n', json.dumps(vars(args), indent=4, separators=(',', ':')))  # pretty print args
+    trainer = TSCAN_trainer(args)
+    trainer.train()
