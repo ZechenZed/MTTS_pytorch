f700af4278ebf5d96b3d9a8daaa8b04cfe056c94
ZechenZed <fluffyzed2@gmail.com>
2023-08-01 11:48:31 -0400

    change the model to origin architecturepath+'

--- plot.py
+++ plot.py
@@ -21,28 +21,28 @@ path = '/edrive2/zechenzh/preprocessed_v4v_minibatch/'
 # np.save(path + 'train_frames_face_large.npy', frames)
 # np.save(path + 'train_BP_systolic_a25.npy', BP)
 
-valid_frames = np.load(path+'valid_frames_face_large.npy')
-valid_BP = np.load(path+'valid_BP_systolic_a25.npy')
-
-frames = valid_frames.reshape((-1, 6, 36, 36))
-BP = valid_BP.reshape(-1)
-ind_BP = np.where(BP == 0)[0][0]
-frames = frames[0:ind_BP]
-BP = BP[0:ind_BP]
-frames = frames.reshape((-1, 10, 6, 36, 36))
-BP = BP.reshape((-1, 10))
-np.save(path + 'valid_frames_face_large.npy', frames)
-np.save(path + 'valid_BP_systolic_a25.npy', BP)
-
-# test_frames = np.load(path+'test_frames_face_large.npy')
-# test_BP = np.load(path+'test_BP_systolic_a25.npy')
+# valid_frames = np.load(path+'valid_frames_face_large.npy')
+# valid_BP = np.load(path+'valid_BP_systolic_a25.npy')
 #
-# frames = test_frames.reshape((-1, 6, 36, 36))
-# BP = test_BP.reshape(-1)
+# frames = valid_frames.reshape((-1, 6, 36, 36))
+# BP = valid_BP.reshape(-1)
 # ind_BP = np.where(BP == 0)[0][0]
 # frames = frames[0:ind_BP]
 # BP = BP[0:ind_BP]
 # frames = frames.reshape((-1, 10, 6, 36, 36))
 # BP = BP.reshape((-1, 10))
-# np.save(path + 'test_frames_face_large.npy', frames)
-# np.save(path + 'test_BP_systolic_a25.npy', BP)
+# np.save(path + 'valid_frames_face_large.npy', frames)
+# np.save(path + 'valid_BP_systolic_a25.npy', BP)
+
+test_frames = np.load(path+'test_frames_face_large.npy')
+test_BP = np.load(path+'test_BP_systolic_a25.npy')
+
+frames = test_frames.reshape((-1, 6, 36, 36))
+BP = test_BP.reshape(-1)
+ind_BP = np.where(BP == 0)[0][0]
+frames = frames[0:ind_BP]
+BP = BP[0:ind_BP]
+frames = frames.reshape((-1, 10, 6, 36, 36))
+BP = BP.reshape((-1, 10))
+np.save(path + 'test_frames_face_large.npy', frames)
+np.save(path + 'test_BP_systolic_a25.npy', BP)
