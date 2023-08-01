fa008d832b91e6d7cd97a1b1733b97f4d870729e
ZechenZed <fluffyzed2@gmail.com>
2023-08-01 10:18:09 -0400

    change the model to origin architecture

--- plot.py
+++ plot.py
@@ -6,30 +6,43 @@ from scipy.ndimage import gaussian_filter, percentile_filter
 from sklearn.metrics import mean_squared_error
 from scipy.signal import butter, filtfilt
 
-valid = np.load('C:/Users/Zed/Desktop/V4V/preprocessed_v4v/train_BP_systolic_a25.npy')
-video = np.load('C:/Users/Zed/Desktop/V4V/preprocessed_v4v/train_frames_face_large.npy')
-print(valid.shape[0] == video.shape[0])
-valid = valid.reshape(-1)
-fs = 25
-plt.plot(valid, label='before')
-plt.show()
+# path = 'C:/Users/Zed/Desktop/V4V/preprocessed_v4v/'
+path = '/edrive2/zechenzh/preprocessed_v4v_minibatch/'
+train_frames = np.load(path + 'train_frames_face_large.npy')
+train_BP = np.load(path + 'train_BP_systolic_a25.npy')
 
-# data_folder_path = "C:/Users/Zed/Desktop/V4V/"
-# video_folder_path = f'{data_folder_path}Phase1_data/Videos/train/'
-# BP_folder_path = f'{data_folder_path}Phase1_data/Ground_truth/BP_raw_1KHz/'
-# # video_folder_path = f'{data_folder_path}Phase2_data/Videos/test/'
-# # BP_folder_path = f'{data_folder_path}Phase2_data/blood_pressure/test_set_bp/'
-# # video_folder_path = f'{data_folder_path}Phase1_data/Videos/valid/'
-# # BP_folder_path = f'{data_folder_path}Phase2_data/blood_pressure/val_set_bp/'
-# # valid_120 = np.load('C:/Users/Zed/Desktop/V4V/preprocessed_v4v/test_BP_systolic_a120.npy')
-#
-# ############## Systolic BP Extraction ##############
-# BP_file_path = []
-# for path in sorted(os.listdir(BP_folder_path)):
-#     if os.path.isfile(os.path.join(BP_folder_path, path)):
-#         BP_file_path.append(path)
-#
-# for i in range(724):
-#     temp_BP = np.loadtxt(BP_folder_path + BP_file_path[i])  # BP loading
-#     if min(temp_BP) < 0:
-#         print(f'warning {BP_file_path[i]} is incorrect')
+frames = train_frames.reshape((-1, 6, 36, 36))
+BP = train_BP.reshape(-1)
+ind_BP = np.where(BP == 0)[0][0]
+frames = frames[0:ind_BP]
+BP = BP[0:ind_BP]
+frames = frames.reshape((-1, 10, 6, 36, 36))
+BP = BP.reshape((-1, 10))
+np.save(path + 'train_frames_face_large.npy', frames)
+np.save(path + 'train_BP_systolic_a25.npy', BP)
+
+valid_frames = np.load('C:/Users/Zed/Desktop/V4V/preprocessed_v4v/valid_frames_face_large.npy')
+valid_BP = np.load('C:/Users/Zed/Desktop/V4V/preprocessed_v4v/valid_BP_systolic_a25.npy')
+
+frames = valid_frames.reshape((-1, 6, 36, 36))
+BP = valid_BP.reshape(-1)
+ind_BP = np.where(BP == 0)[0][0]
+frames = frames[0:ind_BP]
+BP = BP[0:ind_BP]
+frames = frames.reshape((-1, 10, 6, 36, 36))
+BP = BP.reshape((-1, 10))
+np.save(path + 'valid_frames_face_large.npy', frames)
+np.save(path + 'valid_BP_systolic_a25.npy', BP)
+
+test_frames = np.load('C:/Users/Zed/Desktop/V4V/preprocessed_v4v/test_frames_face_large.npy')
+test_BP = np.load('C:/Users/Zed/Desktop/V4V/preprocessed_v4v/test_BP_systolic_a25.npy')
+
+frames = test_frames.reshape((-1, 6, 36, 36))
+BP = test_BP.reshape(-1)
+ind_BP = np.where(BP == 0)[0][0]
+frames = frames[0:ind_BP]
+BP = BP[0:ind_BP]
+frames = frames.reshape((-1, 10, 6, 36, 36))
+BP = BP.reshape((-1, 10))
+np.save(path + 'test_frames_face_large.npy', frames)
+np.save(path + 'test_BP_systolic_a25.npy', BP)
