cabf9a568cec8cdac1acec4b68f4db861e43dc84
ZechenZed <fluffyzed2@gmail.com>
2023-08-06 17:08:41 -0400

    hyperparameter tuning

--- plot.py
+++ plot.py
@@ -15,12 +15,12 @@ frames = train_frames.reshape((-1, 6, 36, 36))
 BP = train_BP.reshape(-1)
 ind_BP = np.where(BP == 0)
 print(ind_BP)
-# frames = frames[0:ind_BP]
-# BP = BP[0:ind_BP]
-# frames = frames.reshape((-1, 10, 6, 36, 36))
-# BP = BP.reshape((-1, 10))
-# np.save(path + 'train_frames_face_large.npy', frames)
-# np.save(path + 'train_BP_systolic_a25.npy', BP)
+frames = frames[0:ind_BP]
+BP = BP[0:ind_BP]
+frames = frames.reshape((-1, 10, 6, 36, 36))
+BP = BP.reshape((-1, 10))
+np.save(path + 'train_frames_face_large.npy', frames)
+np.save(path + 'train_BP_systolic_a25.npy', BP)
 
 valid_frames = np.load(path+'valid_frames_face_large.npy')
 valid_BP = np.load(path+'valid_BP_systolic_a25.npy')
@@ -29,12 +29,12 @@ frames = valid_frames.reshape((-1, 6, 36, 36))
 BP = valid_BP.reshape(-1)
 ind_BP = np.where(BP == 0)
 print(ind_BP)
-# frames = frames[0:ind_BP]
-# BP = BP[0:ind_BP]
-# frames = frames.reshape((-1, 10, 6, 36, 36))
-# BP = BP.reshape((-1, 10))
-# np.save(path + 'valid_frames_face_large.npy', frames)
-# np.save(path + 'valid_BP_systolic_a25.npy', BP)
+frames = frames[0:ind_BP]
+BP = BP[0:ind_BP]
+frames = frames.reshape((-1, 10, 6, 36, 36))
+BP = BP.reshape((-1, 10))
+np.save(path + 'valid_frames_face_large.npy', frames)
+np.save(path + 'valid_BP_systolic_a25.npy', BP)
 
 test_frames = np.load(path+'test_frames_face_large.npy')
 test_BP = np.load(path+'test_BP_systolic_a25.npy')
@@ -43,9 +43,9 @@ frames = test_frames.reshape((-1, 6, 36, 36))
 BP = test_BP.reshape(-1)
 ind_BP = np.where(BP == 0)
 print(ind_BP)
-# frames = frames[0:ind_BP]
-# BP = BP[0:ind_BP]
-# frames = frames.reshape((-1, 10, 6, 36, 36))
-# BP = BP.reshape((-1, 10))
-# np.save(path + 'test_frames_face_large.npy', frames)
-# np.save(path + 'test_BP_systolic_a25.npy', BP)
+frames = frames[0:ind_BP]
+BP = BP[0:ind_BP]
+frames = frames.reshape((-1, 10, 6, 36, 36))
+BP = BP.reshape((-1, 10))
+np.save(path + 'test_frames_face_large.npy', frames)
+np.save(path + 'test_BP_systolic_a25.npy', BP)
