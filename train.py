7b190f7cb8d87e545b75f501e0a8f5a7dc1c923b
ZechenZed <fluffyzed2@gmail.com>
2023-08-09 23:26:39 -0400

    hyperparameter tuning

--- train.py
+++ train.py
@@ -309,7 +309,7 @@ if __name__ == '__main__':
                         help='learning rate')
     parser.add_argument('-fd', '--frame_depth', type=int, default=10,
                         help='frame depth')
-    parser.add_argument('--drop_rate1', type=float, default=0.375,
+    parser.add_argument('--drop_rate1', type=float, default=0.0625,
                         help='Drop rate 1')
     parser.add_argument('--drop_rate2', type=float, default=0.5,
                         help='Drop rate 2')
