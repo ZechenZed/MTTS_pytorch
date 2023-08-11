1667f93bc475c140c80148ec14cc4d0336aeaf65
ZechenZed <fluffyzed2@gmail.com>
2023-08-11 13:05:55 -0400

    hyperparameter tuning

--- train.py
+++ train.py
@@ -311,11 +311,11 @@ if __name__ == '__main__':
                         help='frame depth')
     parser.add_argument('--drop_rate1', type=float, default=0.0625,
                         help='Drop rate 1')
-    parser.add_argument('--drop_rate2', type=float, default=0.5,
+    parser.add_argument('--drop_rate2', type=float, default=0.25,
                         help='Drop rate 2')
     parser.add_argument('--nb_filter1', type=int, default=32,
                         help='number of filter 1')
-    parser.add_argument('--nb_filter2', type=int, default=32,
+    parser.add_argument('--nb_filter2', type=int, default=64,
                         help='number of filter 2')
     parser.add_argument('--nb_dense', type=int, default=384,
                         help='Number of dense layer')
