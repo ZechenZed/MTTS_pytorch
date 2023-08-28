3d7ca0f4d69125d52f7c7eecd3300ca724a0ef9c
ZechenZed <fluffyzed2@gmail.com>
2023-08-28 13:33:54 -0400

    Added Unsupervised method

--- train.py
+++ train.py
@@ -312,7 +312,7 @@ if __name__ == '__main__':
                         help='Drop rate 1')
     parser.add_argument('--drop_rate2', type=float, default=0.5,
                         help='Drop rate 2')
-    parser.add_argument('--nb_filter1', type=int, default=48,
+    parser.add_argument('--nb_filter1', type=int, default=56,
                         help='number of filter 1')
     parser.add_argument('--nb_filter2', type=int, default=64,
                         help='number of filter 2')
