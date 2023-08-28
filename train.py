31e032ab0b722dd99334619e9c79e14cd2bd2357
ZechenZed <fluffyzed2@gmail.com>
2023-08-28 18:31:40 -0400

    Added Unsupervised method

--- train.py
+++ train.py
@@ -310,13 +310,13 @@ if __name__ == '__main__':
                         help='frame depth')
     parser.add_argument('--drop_rate1', type=float, default=0.25,
                         help='Drop rate 1')
-    parser.add_argument('--drop_rate2', type=float, default=0.5,
+    parser.add_argument('--drop_rate2', type=float, default=0.25,
                         help='Drop rate 2')
     parser.add_argument('--nb_filter1', type=int, default=48,
                         help='number of filter 1')
     parser.add_argument('--nb_filter2', type=int, default=64,
                         help='number of filter 2')
-    parser.add_argument('--nb_dense', type=int, default=384,
+    parser.add_argument('--nb_dense', type=int, default=128,
                         help='Number of dense layer')
     parser.add_argument('--best', type=int, default=14,
                         help='Best Epoch')
