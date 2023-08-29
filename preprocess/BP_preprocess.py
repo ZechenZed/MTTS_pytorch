33824d4b76d62a4c6f87d3fdd64dce6a2d513a19
ZechenZed <fluffyzed2@gmail.com>
2023-08-29 16:11:51 -0400

    BP file change

--- preprocess/BP_preprocess.py
+++ preprocess/BP_preprocess.py
@@ -144,7 +144,7 @@ def only_BP(data_type, device_type, image=str(), dim=36):
     for path in sorted(os.listdir(video_folder_path)):
         if os.path.isfile(os.path.join(video_folder_path, path)):
             video_file_path.append(path)
-    # video_file_path = video_file_path[0:1]
+    video_file_path = video_file_path[0:10]
     num_video = len(video_file_path)
     print('Processing ' + str(num_video) + ' Videos')
 
