9374dd6c2a86bc3c287209b27cc828721c576512
ZechenZed <fluffyzed2@gmail.com>
2023-08-29 16:14:11 -0400

    BP file change

--- PTT_method.py
+++ PTT_method.py
@@ -40,7 +40,7 @@ def PTT():
     plt.plot(chrome_fingerBVP, label='Chrome finger')
     plt.plot(ICA_faceBVP, label='ICA face')
     plt.plot(ICA_fingerBVP, label='ICA finger')
-    plt.savefig(env_path+'PTTplot.png')
+    plt.savefig(env_path+'PTTplot.png',dpi=2400)
     plt.legend()
     plt.show()
     #
