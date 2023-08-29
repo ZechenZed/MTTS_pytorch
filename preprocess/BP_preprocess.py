a2929767e5cfc93aeaa1e476acaf3b2fc1c8e0f4
ZechenZed <fluffyzed2@gmail.com>
2023-08-29 16:19:29 -0400

    BP file change

--- preprocess/BP_preprocess.py
+++ preprocess/BP_preprocess.py
@@ -206,7 +206,7 @@ def only_BP(data_type, device_type, image=str(), dim=36):
         BP_lf[frame_ind:frame_ind + current_frames] = temp_BP_lf_systolic_inter
         frame_ind += current_frames
 
-    fig = plt.figure(figsize=(200, 180))
+    fig = plt.figure(figsize=(10, 8))
     plt.plot(BP_lf, label='original')
     # plt.plot(BP_lf_25, label='sigma = 25')
     # plt.plot(BP_lf_120, label='sigma = 120')
