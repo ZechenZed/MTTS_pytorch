e97152bcfe57baeaef83991972148caf42e960ce
ZechenZed <fluffyzed2@gmail.com>
2023-08-30 07:47:32 -0400

    ckpts

--- train.py
+++ train.py
@@ -308,13 +308,13 @@ if __name__ == '__main__':
                         help='learning rate')
     parser.add_argument('-fd', '--frame_depth', type=int, default=10,
                         help='frame depth')
-    parser.add_argument('--drop_rate1', type=float, default=0.25,
+    parser.add_argument('--drop_rate1', type=float, default=0.1,
                         help='Drop rate 1')
-    parser.add_argument('--drop_rate2', type=float, default=0.75,
+    parser.add_argument('--drop_rate2', type=float, default=0.2,
                         help='Drop rate 2')
-    parser.add_argument('--nb_filter1', type=int, default=48,
+    parser.add_argument('--nb_filter1', type=int, default=16,
                         help='number of filter 1')
-    parser.add_argument('--nb_filter2', type=int, default=64,
+    parser.add_argument('--nb_filter2', type=int, default=32,
                         help='number of filter 2')
     parser.add_argument('--nb_dense', type=int, default=256,
                         help='Number of dense layer')
