a2810ba0f978534aad8ffe37b1dc8bd7456e93df
ZechenZed <fluffyzed2@gmail.com>
2023-08-30 07:05:17 -0400

    ckpts

--- plot.py
+++ plot.py
@@ -7,37 +7,39 @@ from sklearn.metrics import mean_squared_error
 from scipy.signal import butter, filtfilt
 
 # path = 'C:/Users/Zed/Desktop/V4V/preprocessed_v4v/'
-path = '/edrive2/zechenzh/preprocessed_v4v_minibatch/'
+path = '/edrive2/zechenzh/preprocessed_v4v/'
 
-# train_frames = np.load(path + 'train_frames_face_large.npy')
-# train_BP = np.load(path + 'train_BP_systolic_a25.npy')
-#
-# frames = train_frames.reshape((-1, 6, 36, 36))
-# BP = train_BP.reshape(-1)
-#
-# ind_BP = np.where(BP == 0)[0][0]
-# print(ind_BP)
-# frames = frames[0:ind_BP]
-# BP = BP[0:ind_BP]
-# frames = frames.reshape((-1, 10, 6, 36, 36))
-# BP = BP.reshape((-1, 10))
-# np.save(path + 'train_frames_face_large.npy', frames)
-# np.save(path + 'train_BP_systolic_a25.npy', BP)
-#
-# valid_frames = np.load(path+'valid_frames_face_large.npy')
-# valid_BP = np.load(path+'valid_BP_systolic_a25.npy')
-#
-# frames = valid_frames.reshape((-1, 6, 36, 36))
-# BP = valid_BP.reshape(-1)
-#
-# ind_BP = np.where(BP == 0)[0][0]
-# print(ind_BP)
-# frames = frames[0:ind_BP]
-# BP = BP[0:ind_BP]
-# frames = frames.reshape((-1, 10, 6, 36, 36))
-# BP = BP.reshape((-1, 10))
-# np.save(path + 'valid_frames_face_large.npy', frames)
-# np.save(path + 'valid_BP_systolic_a25.npy', BP)
+train_frames = np.load(path + 'train_frames_face_large.npy')
+train_BP = np.load(path + 'train_BP_systolic_a25.npy')
+
+frames = train_frames.reshape((-1, 6, 36, 36))
+BP = train_BP.reshape(-1)
+
+ind_BP = np.where(BP == 0)[0][0]
+print(ind_BP)
+frames = frames[0:ind_BP]
+BP = BP[0:ind_BP]
+plt.plot(BP)
+plt.savefig('/edrive2/zechenzh/BP.jpg', dpi=1200)
+frames = frames.reshape((-1, 10, 6, 36, 36))
+BP = BP.reshape((-1, 10))
+np.save(path + 'train_frames_face_large.npy', frames)
+np.save(path + 'train_BP_systolic_a25.npy', BP)
+
+valid_frames = np.load(path+'valid_frames_face_large.npy')
+valid_BP = np.load(path+'valid_BP_systolic_a25.npy')
+
+frames = valid_frames.reshape((-1, 6, 36, 36))
+BP = valid_BP.reshape(-1)
+
+ind_BP = np.where(BP == 0)[0][0]
+print(ind_BP)
+frames = frames[0:ind_BP]
+BP = BP[0:ind_BP]
+frames = frames.reshape((-1, 10, 6, 36, 36))
+BP = BP.reshape((-1, 10))
+np.save(path + 'valid_frames_face_large.npy', frames)
+np.save(path + 'valid_BP_systolic_a25.npy', BP)
 #
 test_frames = np.load(path+'test_frames_face_large.npy')
 test_BP = np.load(path+'test_BP_systolic_a25.npy')
