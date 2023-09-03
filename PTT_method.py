1cc7c7337fa5101cbf1e2da41fbeea256064e4b5
ZechenZed <fluffyzed2@gmail.com>
2023-09-02 21:13:51 -0400

    ckpts

--- PTT_method.py
+++ PTT_method.py
@@ -30,14 +30,15 @@ def video_process(device_type):
     for video in video_file_path:
         finger_frames = preprocess_finger(env_path + 'Dual_Camera/finger/' + video)
         frames, fps = preprocess_raw_video_unsupervised(face_video_folder_path + video)
-        np.save(f'{env_path}/preprocessed_DC/face/{video}.npy', frames)
-        np.save(f'{env_path}/preprocessed_DC/finger/{video}.npy', finger_frames)
+        np.save(f'{env_path}/preprocessed_DC/face/{video[0:4]}.npy', frames)
+        np.save(f'{env_path}/preprocessed_DC/finger/{video[0:4]}.npy', finger_frames)
+
 
 def PTT(device_type):
     if device_type == "local":
-        env_path = 'C:/Users/Zed/Desktop/Dual_Camera/'
+        env_path = 'C:/Users/Zed/Desktop/preprocessed_DC/'
     else:
-        env_path = '/edrive2/zechenzh/dual_camera/'
+        env_path = '/edrive2/zechenzh/preprocessed_DC/'
 
     face_video_folder_path = env_path + 'face/'
     video_file_path = []
@@ -45,46 +46,45 @@ def PTT(device_type):
         if os.path.isfile(os.path.join(face_video_folder_path, path)):
             video_file_path.append(path)
 
-    # bio_path = 'C:/Users/Zed/Desktop/Dual_Camera/bp/S55.csv'
-    # bio = pd.read_csv(bio_path)['SystolicBP']
-
-    # finger_frames = preprocess_finger(finger_video_path)
-    # frames, fps = preprocess_raw_video_unsupervised(face_video_path)
-    #
-    # np.save(env_path+'/face/Subject S055 3487906/s55_face.npy', frames)
-    # np.save(env_path+'/finger/Subject S055 3487906/s55_finger.npy', finger_frames)
+    print(video_file_path)
 
     print('Loading Face Frames')
-    face_frames = np.load(env_path + 'face/s55_face.npy')[5000:-1]
+    face_frames = np.load(env_path + 'face/S055.npy')
     print('Loading Finger Frames')
-    finger_frames = np.load(env_path + 'finger/s55_finger.npy')[5000:-1]
+    finger_frames = np.load(env_path + 'finger/S055.npy')
+
+    start_frame = 5000
+    end_frame = min(len(face_frames), len(finger_frames))
+
+    face_frames = face_frames[start_frame:end_frame]
+    finger_frames = finger_frames[start_frame:end_frame]
 
     print('Processing CHROME')
     chrome_faceBVP = CHROME_DEHAAN(face_frames, 240)
     chrome_fingerBVP = CHROME_DEHAAN(finger_frames, 240)
 
-    # print('Processing ICA')
-    # ICA_faceBVP = ICA_POH(face_frames, 240)
-    # ICA_fingerBVP = ICA_POH(finger_frames, 240)
-    # print('Ending ICA')
+    ICA_faceBVP = ICA_POH(face_frames, 240)
+    ICA_fingerBVP = ICA_POH(finger_frames, 240)
+
     fig = plt.figure(figsize=(20, 18))
     print('Plotting figures')
+
     plt.plot(chrome_faceBVP, label='Chrome face')
     plt.plot(chrome_fingerBVP, label='Chrome finger')
+    plt.plot(ICA_fingerBVP, label='ICA finger')
+    plt.plot(ICA_faceBVP, label='ICA face')
     plt.legend()
-
-    # plt.plot(ICA_faceBVP, label='ICA face')
-    # plt.plot(ICA_fingerBVP, label='ICA finger')
-
-    print('Saving plot')
-    plt.savefig(env_path + 'PTTplot.png',dpi=1200)
     plt.show()
 
-    # peaks, _ = find_peaks(BVP, distance=5)
-    # finger_peaks, _ = find_peaks(finger_BVP, distance=5)
+    chrome_peaks, _ = find_peaks(chrome_faceBVP, distance=5)
+    chorme_finger_peaks, _ = find_peaks(chrome_fingerBVP, distance=5)
+    ICA_peaks, _ = find_peaks(chrome_faceBVP, distance=5)
+    ICA_finger_peaks, _ = find_peaks(chrome_fingerBVP, distance=5)
+    if len(ICA_peaks) == len(ICA_finger_peaks):
+        print('synchronized')
 
 
 if __name__ == '__main__':
-    device_type = 'remote'
-    # PTT(device_type)
-    video_process(device_type)
+    device_type = 'local'
+    PTT(device_type)
+    # video_process(device_type)
