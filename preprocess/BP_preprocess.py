64eee6cfda37d3d16cb0fdea1b7e77f6fbe462c5
ZechenZed <fluffyzed2@gmail.com>
2023-09-09 00:38:20 -0400

    minor change of number of files processed in BP file

--- preprocess/BP_preprocess.py
+++ preprocess/BP_preprocess.py
@@ -34,7 +34,7 @@ def data_process(data_type, device_type, image=str(), dim=36):
         if os.path.isfile(os.path.join(video_folder_path, path)):
             video_file_path.append(path)
 
-    video_file_path = video_file_path[15:25]
+    # video_file_path = video_file_path[15:25]
     num_video = len(video_file_path)
     print('Processing ' + str(num_video) + ' Videos')
 
@@ -52,7 +52,7 @@ def data_process(data_type, device_type, image=str(), dim=36):
     for path in sorted(os.listdir(BP_folder_path)):
         if os.path.isfile(os.path.join(BP_folder_path, path)):
             BP_file_path.append(path)
-    BP_file_path = BP_file_path[15:25]
+    # BP_file_path = BP_file_path[15:25]
 
     frames = np.zeros(shape=(tt_frame, 6, dim, dim))
     BP_lf = np.zeros(shape=tt_frame)
@@ -234,7 +234,7 @@ def only_BP(data_type, device_type, image=str(), dim=36):
 
 
 if __name__ == '__main__':
-    data_process('valid', 'local', 'face_large')
+    data_process('valid', 'remote', 'face_large')
     # data_process('train', 'remote', 'face_large')
     # data_process('test', 'remote', 'face_large')
     # only_BP('train', 'remote', 'face_large')
