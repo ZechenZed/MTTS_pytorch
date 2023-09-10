5c6602e0c10978e5a5944230d1a7ac385fdeeb82
ZechenZed <fluffyzed2@gmail.com>
2023-09-09 23:58:10 -0400

    minor change of number of files processed in BP file

--- train.py
+++ train.py
@@ -304,15 +304,15 @@ if __name__ == '__main__':
                         help='learning rate')
     parser.add_argument('-fd', '--frame_depth', type=int, default=10,
                         help='frame depth')
-    parser.add_argument('--drop_rate1', type=float, default=0.25,
+    parser.add_argument('--drop_rate1', type=float, default=0.1,
                         help='Drop rate 1')
-    parser.add_argument('--drop_rate2', type=float, default=0.5,
+    parser.add_argument('--drop_rate2', type=float, default=0.25,
                         help='Drop rate 2')
     parser.add_argument('--nb_filter1', type=int, default=32,
                         help='number of filter 1')
     parser.add_argument('--nb_filter2', type=int, default=64,
                         help='number of filter 2')
-    parser.add_argument('--nb_dense', type=int, default=128,
+    parser.add_argument('--nb_dense', type=int, default=256,
                         help='Number of dense layer')
     parser.add_argument('--best', type=int, default=14,
                         help='Best Epoch')
