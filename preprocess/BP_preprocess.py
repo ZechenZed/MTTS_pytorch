151a45ccbd9c3bac30a300081d04a311c28bed98
ZechenZed <fluffyzed2@gmail.com>
2023-09-13 16:58:35 -0400

    New way for face detection using mediapipe

--- preprocess/BP_preprocess.py
+++ preprocess/BP_preprocess.py
@@ -38,7 +38,7 @@ def data_process(data_type, device_type, image=str(), dim=36):
     num_video = len(video_file_path)
     print('Processing ' + str(num_video) + ' Videos')
 
-    videos = [Parallel(n_jobs=8)(
+    videos = [Parallel(n_jobs=20)(
         delayed(preprocess_raw_video)(video_folder_path + video, dim) for video in video_file_path)]
     videos = videos[0]
 
