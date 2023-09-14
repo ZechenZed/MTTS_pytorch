7c911f8fd387a71d35a9e16a5673300ad6783e9d
ZechenZed <fluffyzed2@gmail.com>
2023-09-13 23:22:32 -0400

    New way for face detection using mediapipe

--- preprocess/BP_preprocess.py
+++ preprocess/BP_preprocess.py
@@ -34,7 +34,7 @@ def data_process(data_type, device_type, image=str(), dim=36):
         if os.path.isfile(os.path.join(video_folder_path, path)):
             video_file_path.append(path)
 
-    video_file_path = video_file_path[0:2]
+    # video_file_path = video_file_path[0:2]
     num_video = len(video_file_path)
     print('Processing ' + str(num_video) + ' Videos')
 
@@ -235,7 +235,7 @@ def only_BP(data_type, device_type, image=str(), dim=36):
 
 
 if __name__ == '__main__':
-    data_process('valid', 'local', 'face_large')
+    data_process('valid', 'remote', 'face_large')
     # data_process('train', 'remote', 'face_large')
     # data_process('test', 'remote', 'face_large')
     # only_BP('train', 'remote', 'face_large')
