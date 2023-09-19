353bf72eba191159bcc13e32f97eaf488f8be232
ZechenZed <fluffyzed2@gmail.com>
2023-09-18 21:58:01 -0400

    Sweep function

--- preprocess/video_preprocess.py
+++ preprocess/video_preprocess.py
@@ -39,9 +39,11 @@ def preprocess_raw_video(video_file_path, dim=72, plot=True, face_crop=True):
     success, img = vidObj.read()
     height, width, _ = img.shape
     # print(f'Height:{height}, width{width}')
-    # face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    # face_detection = mp.solutions.face_detection.FaceDetection(min_detection_confidence=0.2)
+    
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    face_detection = mp.solutions.face_detection.FaceDetection(min_detection_confidence=0.2)
     prev_roi = img_as_float(img)
+
     ############## Reading frame by frame ##############
     while success and is_not_consecutive(invalid_frames, 25, totalFrames):
         t.append(vidObj.get(cv2.CAP_PROP_POS_MSEC))
@@ -68,40 +70,38 @@ def preprocess_raw_video(video_file_path, dim=72, plot=True, face_crop=True):
         #     # cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
         #
         #     roi = img_as_float(img[y:y + h, x:x + w, :])
-
-        ########## Face detection with OPENCV #############
         # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-        # ########## Face detection with MediaPipe ############
-        # results = face_detection.process(img)
-        # roi = 0
-        # if results.detections:
-        #     for face in results.detections:
-        #         bounding_box = face.location_data.relative_bounding_box
-        #         x = int(bounding_box.xmin * img.shape[1])
-        #         w = int(bounding_box.width * img.shape[1])
-        #         y = int(bounding_box.ymin * img.shape[0])
-        #         h = int(bounding_box.height * img.shape[0])
-        #
-        #         cv2.rectangle(img, (x, int(y - 0.2 * h)), (x + w, y + h), (0, 255, 0), 2)
-        #         roi = img_as_float(img[int(y - 0.2 * h):y + h, x:x + w, :])
-        # else:
-        #     invalid_frames.append(i)
-        #     print(f'No Face Detected in {video_file_path[-12:]} at {i}th Frame')
-        #
-        # ##### Video #######
-        # cv2.imshow('Frame', img)
-        # # Press 'q' to quit
-        # if cv2.waitKey(1) & 0xFF == ord('q'):
-        #     break
-        #
-        # try:
-        #     vidLxL = cv2.resize(roi, (dim, dim), interpolation=cv2.INTER_LINEAR)
-        #     prev_roi = roi
-        # except:
-        #     invalid_frames.append(i)
-        #     print(f'Exception triggered in {video_file_path[-12:]} at frame {i}')
-        #     vidLxL = cv2.resize(prev_roi, (dim, dim), interpolation=cv2.INTER_LINEAR)
+        ########## Face detection with MediaPipe ############
+        results = face_detection.process(img)
+        roi = 0
+        if results.detections:
+            for face in results.detections:
+                bounding_box = face.location_data.relative_bounding_box
+                x = int(bounding_box.xmin * img.shape[1])
+                w = int(bounding_box.width * img.shape[1])
+                y = int(bounding_box.ymin * img.shape[0])
+                h = int(bounding_box.height * img.shape[0])
+
+                cv2.rectangle(img, (x, int(y - 0.2 * h)), (x + w, y + h), (0, 255, 0), 2)
+                roi = img_as_float(img[int(y - 0.2 * h):y + h, x:x + w, :])
+        else:
+            invalid_frames.append(i)
+            print(f'No Face Detected in {video_file_path[-12:]} at {i}th Frame')
+
+        ##### Video #######
+        cv2.imshow('Frame', img)
+        # Press 'q' to quit
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        try:
+            vidLxL = cv2.resize(roi, (dim, dim), interpolation=cv2.INTER_LINEAR)
+            prev_roi = roi
+        except:
+            invalid_frames.append(i)
+            print(f'Exception triggered in {video_file_path[-12:]} at frame {i}')
+            vidLxL = cv2.resize(prev_roi, (dim, dim), interpolation=cv2.INTER_LINEAR)
         # vidLxL = cv2.rotate(vidLxL, cv2.ROTATE_90_CLOCKWISE)
         vidLxL = cv2.cvtColor(vidLxL.astype('float32'), cv2.COLOR_BGR2RGB)
         vidLxL[vidLxL > 1] = 1
