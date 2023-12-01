3bc1c000981fe091b1fb2b0d26d7ee4d45bf6f19
ZechenZed <fluffyzed2@gmail.com>
2023-12-01 10:31:27 -0500

    One way anova

--- train.py
+++ train.py
@@ -349,15 +349,15 @@ if __name__ == '__main__':
                         help='nb_batch')
     parser.add_argument('--kernel', type=int, default=3,
                         help='Kernel size')
-    parser.add_argument('--lr', type=float, default=0.001,
+    parser.add_argument('--lr', type=float, default=0.0005,
                         help='learning rate')
     parser.add_argument('--frame_depth', type=int, default=10,
                         help='frame depth')
-    parser.add_argument('--dropout_rate1', type=float, default=0.9,
+    parser.add_argument('--dropout_rate1', type=float, default=0.4481476982233055,
                         help='Drop rate 1')
-    parser.add_argument('--dropout_rate2', type=float, default=0.8,
+    parser.add_argument('--dropout_rate2', type=float, default=0.5822231169515606,
                         help='Drop rate 2')
-    parser.add_argument('--nb_filter1', type=int, default=32,
+    parser.add_argument('--nb_filter1', type=int, default=8,
                         help='number of filter 1')
     parser.add_argument('--nb_filter2', type=int, default=64,
                         help='number of filter 2')
