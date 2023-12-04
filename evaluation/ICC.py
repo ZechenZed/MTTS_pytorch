fix the calculation of ICC(1)

evaluation/ICC.py
@@ -1,6 +1,3 @@
-import statsmodels.api as sm
-from statsmodels.formula.api import ols
-import statsmodels.stats.anova as anova
 from scipy.stats import f_oneway
 import pandas as pd
 import time
@@ -10,7 +7,7 @@ import numpy as np
 def ICC(gt, pred):
     var_gt = np.var(gt)
     var_pred = np.var(pred)
-    return var_pred / (var_gt + var_pred)
+    return var_gt / (var_gt + var_pred)
 #
 # if __name__ == '__main__':
 #     gt = [14, 15, 16, 17, 18, 20, 14, 15, 16, 17, 19, 20]